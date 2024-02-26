bc4da502e90f83035fd24a99ac455b18948db674
seongjin96 <w8348@naver.com>
2024-02-26 13:46:07 +0900

    TypeScript Vue import 에러 해결

--- ts-shim.d.ts
+++ ts-shim.d.ts
@@ -1,8 +1,7 @@
 // Typescript Vue 프로젝트에서 .vue 없이 파일을 import 하면 발생하는 이슈 대처법
 // https://univdev.page/posts/vue-typescript-without-extension/
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import Vue from 'vue'
+  export default Vue
 }
