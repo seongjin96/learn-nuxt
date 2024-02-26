4ae0997789334d5cfaba5a440e464f1c3760d2ab
seongjin96 <w8348@naver.com>
2024-02-26 13:52:41 +0900

    nuxt-link 태그

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>{{ $route.name }}페이지</h1>
+    <header>
+      <h1>{{ $route.name }}페이지</h1>
+<!--      router-link와 동일한 역할-->
+      <NuxtLink to="/">홈페이지</NuxtLink>
+      <NuxtLink to="/main">메인 페이지</NuxtLink>
+      <NuxtLink to="/product">상품 페이지</NuxtLink>
+    </header>
     <Nuxt />
   </div>
 </template>
