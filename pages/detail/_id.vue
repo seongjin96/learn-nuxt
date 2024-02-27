8275b6b068dbd90a9a6ff20bfe585a0f49746fd8
seongjin96 <w8348@naver.com>
2024-02-27 17:42:00 +0900

    상품 상세 페이지 레이아웃 정리 및 전역 스타일시트 설정

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <h1>상세 페이지</h1>
-    <div>
-      {{ product }}
-      <img :src="product.imageUrl" :alt="product.name">
-      <p>name: {{ product.name }}</p>
-      <p>price: {{ product.price }}</p>
+    <div class="container">
+      <div class="main-panel">
+        <img
+          class="product-image"
+          :src="product.imageUrl"
+          :alt="product.name"
+        />
+      </div>
+      <div class="side-panel">
+        <p class="name">{{ product.name }}</p>
+        <p class="price">{{ product.price }}</p>
+<!--        <button type="button" @click="addToCart">Add to Cart</button>-->
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +24,7 @@ import { fetchProductById } from '~/api'
 
 export default Vue.extend({
   async asyncData({ params }) {
-    const response = await fetchProductById(params.id)
+    const response: AxiosResponse<any> = await fetchProductById(params.id)
     const product: AxiosResponse<any> = response.data
     return { product }
   }
@@ -30,5 +37,21 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
 </style>
